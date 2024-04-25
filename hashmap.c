1474a46e0fbdfb4bff3659889bb663dfd3706930
andressaave <162830700+andressaave@users.noreply.github.com>
2024-04-25 13:51:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,8 +40,17 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
+  long pos = hash(key,map->capacity);
 
+  while(map->buckets[pos] != NULL && map->buckets[pos]->key != NULL){
+      pos = (pos+1) % map->capacity;
+    }
 
+  map->buckets[pos]->key = key; 
+  map->buckets[pos]->value = value;
+  map->size++;
+
+  map->current = pos;
 }
 
 void enlarge(HashMap * map) {
