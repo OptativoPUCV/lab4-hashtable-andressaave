74cf363f6735c0df16126c922e61e30357890943
andressaave <162830700+andressaave@users.noreply.github.com>
2024-04-27 13:47:33 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -116,12 +116,11 @@ HashMap * createMap(long capacity) {
 
 void eraseMap(HashMap * map,  char * key) {
   Pair *pair = searchMap(map, key);
-  if (pair != NULL) {
       
-      pair->key = NULL;
+  pair->key = NULL;
       
-      map->size--;
-  }
+  map->size--;
+  
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
